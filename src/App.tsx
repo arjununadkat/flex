8f147f7c97c14626c5b2595f8ff61dbc8a869ac9
Kawaki-san <rtkayy123@gmail.com>
2021-02-08 22:16:24 +0200

    creates register form

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Navbar from './components/layout/Navbar'
 import Home from './components/pages/Home';
 import ProductDetails from './components/pages/ProductDetails';
 import SignIn from './components/auth/SignIn';
+import Register from './components/auth/Register';
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/products/:id' component={ProductDetails} />
-          <Route path='/signin' component={SignIn} />
+          <Route path='/admin/signin' component={SignIn} />
+          <Route path='/admin/register' component={Register} />
         </Switch>
       </div>
     </BrowserRouter>
--- src/components/auth/Register.tsx
+++ src/components/auth/Register.tsx
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
 
 class Register extends Component {
+    state = {
+        email: '',
+        password: '',
+        password2: ''
+    }
+
+    handleSubmit = (e: any) => {
+        e.preventDefault();
+        console.log(this.state);
+    };
+    handleChange = (e: any) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    };
+
     render() {
         return (
             <div>
-
+                <form className="mx-auto md:w-1/3 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                    <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Create your account</h2>
+                    <p className="leading-relaxed mb-5 text-gray-600">Please enter your details below</p>
+                    <div className="relative mb-4">
+                        <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
+                        <input onChange={this.handleChange} type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                    </div>
+                    <div className="relative mb-4">
+                        <label htmlFor="password" className="leading-7 text-sm text-gray-600">Password</label>
+                        <input onChange={this.handleChange} type="password" id="password" name="password" className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                    </div>
+                    <div className="relative mb-4">
+                        <label htmlFor="password2" className="leading-7 text-sm text-gray-600">Re-enter password</label>
+                        <input onChange={this.handleChange} type="password" id="password2" name="password2" className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                    </div>
+                    <button className="text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg" type="submit" onClick={this.handleSubmit} >Create Account</button>
+                </form>
             </div>
         )
     }
